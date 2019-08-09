2e3a21f3ba0743c587c433e031ed16a377bc6584
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-09 09:50:08 -0700

    added data

--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 const express = require("express");
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("testing");
-});
+let people = [
+  {
+    id: 1,
+    name: "Bob"
+  },
+  {
+    id: 2,
+    name: "Joe"
+  },
+  {
+    id: 3,
+    name: "Jon"
+  }
+];
+
+let chores = [
+  {
+    id: 1,
+    description: "Wash dishes",
+    assignedTo: 1,
+    completed: false
+  },
+  {
+    id: 2,
+    description: "Vacuum house",
+    assignedTo: 1,
+    completed: false
+  },
+  {
+    id: 3,
+    description: "Buy groceries",
+    assignedTo: 2,
+    completed: false
+  },
+  {
+    id: 4,
+    description: "Take out trash",
+    assignedTo: 2,
+    completed: false
+  },
+  {
+    id: 5,
+    description: "Wash car",
+    assignedTo: 3,
+    completed: false
+  },
+  {
+    id: 6,
+    description: "Wash laundry",
+    assignedTo: 3,
+    completed: false
+  }
+];
 
 module.exports = server;
