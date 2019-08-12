740f902ae9510f30f0fd7d5e7f0cd65bb4ef2df2
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-11 17:47:31 -0700

    tests for heroku

--- server.js
+++ server.js
@@ -57,50 +57,137 @@ let chores = [
   }
 ];
 
-// server.get("/", (req, res) => {
-//   res.send("testing");
-// });
+const choreId = 1;
 
-server.get("/chores", (req, res) => {
-  res.status(200).json(chores);
+// function insert(chore) {
+//   return;
+// }
+
+// function remove(id) {
+//   return;
+// }
+
+server.get("/", (req, res) => {
+  res.send({ hellO: "how are you?" });
 });
 
-// server.get("/people/:id/chores", (req, res) => {
+// server.get("/chores", (req, res) => {
+//   const completed = req.query.completed.id;
+//   console.log(completed);
+
+//   if (completed) {
+//     const filter = completed === "true" ? true : false;
+//     if (compiled === "true") {
+//       filter = true;
+//     }
+//   } else {
+//   }
+
+//   res.status(200).json(chores);
+// });
+
+// server.get("chores/:id", (req, res) => {
+//   // const id = req.params.id
+
+//   // chores.get(id)
+//   // .then(chore => {
+//   //     if (chore) {
+//   //         res.status(200).json(chore)
+//   //     } else {
+//   //         res.status(404).json({ message: "The chore with the specified ID does not exist." })
+//   //     }
+//   // })
+//   // .catch(error => {
+//   //     res.status(500).json({ error: "The chores information could not be retreived "})
+//   // })
+
 //   const id = req.params.id;
 
-//   if (!id) {
-//     res
-//       .status(404)
-//       .json({ message: "The person with the specified ID does not exist." });
-//   } else if (!people.chores) {
-//     res.send([]);
-//   } else {
-//     res.status(200).json(chores.description);
-//   }
+//   const chore = chores.find(chore => chore.id === Number(id));
+
+//   res.status(201).json(chores);
+
+//   //   const id = req.params.id;
+
+//   //   if (!id) {
+//   //     res
+//   //       .status(404)
+//   //       .json({ message: "The person with the specified ID does not exist." });
+//   //   } else if (!people.chores) {
+//   //     res.send([]);
+//   //   } else {
+//   //     res.status(200).json(chores.description);
+//   //   }
 // });
 
 // server.post("/chores", (req, res) => {
-//   const chore = req.body;
+//   //   const chore = req.body;
 
-//   if (!chore.description) {
-//     res
-//       .status(400)
-//       .json({ message: "Please provie a description for the chore." });
-//   } else {
-//     chores.push(chore);
-//     res.status(200).json(chore);
-//   }
+//   //   if (!chore.description) {
+//   //     res
+//   //       .status(400)
+//   //       .json({ message: "Please provide a description for the chore." });
+//   //   } else {
+//   //     chores
+//   //       .insert(chore)
+//   //       .then(chore => {
+//   //         res.status(201).json(chore);
+//   //       })
+//   //       .catch(error => {
+//   //         res
+//   //           .status(500)
+//   //           .json({ message: "There was an error adding the chore." });
+//   //       });
+//   //   }
+
+//   let chore = req.body;
+
+//   chore.id = choreId++;
+
+//   chores.push(chore);
+
+//   res.status(201).json(chores);
+
+//   //   if (!chore.description) {
+//   //     res
+//   //       .status(400)
+//   //       .json({ message: "Please provie a description for the chore." });
+//   //   } else {
+//   //     chores.push(chore);
+//   //     res.status(200).json(chore);
+//   //   }
 // });
 
 // server.delete("/chores/:id", (req, res) => {
+//   //   const id = req.params.id;
+
+//   //   chores
+//   //     .remove(id)
+//   //     .then(chore => {
+//   //       if (chore) {
+//   //         res.status(200).json(chore);
+//   //       } else {
+//   //         res.status(404).json({
+//   //           message: "The chore with the specified ID does not exist."
+//   //         });
+//   //       }
+//   //     })
+//   //     .catch(error => {
+//   //       res.status(500).json({ message: "The chore could not be removed." });
+//   //     });
+
 //   const id = req.params.id;
 
-//   if (!id) {
-//     res.status(404).json({ message: "There was an error deleting the chore." });
-//   } else {
-//     chores.splice(id, 1);
-//     res.status(201).json(); // =======
-//   }
+//   chores = chores.filter(chore => chore.id !== Number(id));
+
+//   //   const id = req.params.id;
+
+//   //   if (!id) {
+//   //     res.status(404).json({ message: "There was an error deleting the chore." });
+//   //   } else {
+//   //     chores.splice(id, 1);
+//   //     res.status(201).json(); // =======
+//   //   }
 // });
 
 module.exports = server;
